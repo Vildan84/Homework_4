56b05ab13530b10d05ac390eb7bee43ab1995ac7
Vildan <vildan84@mail.ru>
2022-08-03 21:50:07 +0300

    Added quicksort solution

--- Program.cs
+++ Program.cs
@@ -66,6 +66,7 @@ void Task29()
     {
         array[i] = rand.Next(-99, 99);
     }
+    
 
     for (int i = 0; i < array.Length; i++)
     {
@@ -84,5 +85,52 @@ void Task29()
     PrintArray(array);
 }
 
+// Task29();
 
-Task29();
+void fillArray(int[] collection)
+{
+    int size = collection.Length;
+    Random rand = new Random(); 
+    for (int i = 0 ; i < size; i++)
+    {
+        collection[i] = rand.Next(0, 99);
+    }
+}
+
+int[] arr = new int[5];
+fillArray(arr);
+PrintArray(arr);
+
+int partition (int[] array, int start, int end)
+{
+    int temp;
+    int marker = start;
+    for ( int i = start; i < end; i++ ) 
+    {
+        if ( array[i] < array[end] )
+        {
+            temp = array[marker];
+            array[marker] = array[i];
+            array[i] = temp;
+            marker += 1;
+        }
+    }
+    temp = array[marker];
+    array[marker] = array[end];
+    array[end] = temp; 
+    return marker;
+}
+
+void quicksort (int[] array, int start, int end)
+   {
+       if ( start >= end ) 
+       {
+           return;
+       }
+       int pivot = partition (array, start, end);
+       quicksort (array, start, pivot-1);
+       quicksort (array, pivot+1, end);
+   }
+
+quicksort(arr, 0, 4);
+PrintArray(arr);
